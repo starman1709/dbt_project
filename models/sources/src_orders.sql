13c845d6022181f1d0f4d3bb6ae129d79084d9e9
Ratul Ghosh <ghoshratul455@gmail.com>
2024-05-22 10:28:49 +0000

    Final commit

--- models/sources/src_orders.sql
+++ models/sources/src_orders.sql
@@ -1,5 +1,5 @@
 WITH raw_orders AS (
-    SELECT * FROM {{ source('snowflake_sample_data', 'orders') }}
+    SELECT * FROM {{ source('source_data', 'orders') }}
 )
 
 SELECT o_custkey, o_comment FROM raw_orders
